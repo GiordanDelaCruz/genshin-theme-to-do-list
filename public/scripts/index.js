c23329c419d60a62db4604def34cadef5d5679fd
Giordan Dela Cruz <52592435+GiordanDelaCruz@users.noreply.github.com>
2023-10-10 01:11:43 -0400

    Updated look of Ganyu theme.

---
+++ public/scripts/index.js
@@ -0,0 +1,7 @@
+var ganyuThemeButton = document.getElementsByClassName("ganyu-theme-btn");
+
+ganyuThemeButton[0].addEventListner("click", ganyuTheme );
+
+function ganyuTheme(){
+    alert("ganyu theme selected");
+};
