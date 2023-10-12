fb276ee9ceadda033ebb618fd77193ccf7431b75
Giordan Dela Cruz <52592435+GiordanDelaCruz@users.noreply.github.com>
2023-10-12 01:01:35 -0400

    Added some functionality to my character theme buttons. Incomplete, but made good progress. Need to clean documentation of code eventually.

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,7 +1,49 @@
-var ganyuThemeButton = document.getElementsByClassName("ganyu-theme-btn");
-
-ganyuThemeButton[0].addEventListner("click", ganyuTheme );
+// Get reference to all objects
+const dateHeader = document.getElementsByClassName("date-header");
+const menuHeaders = document.getElementsByClassName("menu-header");
+const chibiImages = document.getElementsByClassName("chibi-character");
+const mainContainer = document.getElementsByClassName("main-container");
 
 function ganyuTheme(){
-    alert("ganyu theme selected");
-};
+
+    // Change CSS image source to Ganyu Chibi
+    chibiImages[0].src="/images/ganyu-chibi-4.png";
+    chibiImages[1].src="/images/ganyu-chibi-3.png";
+    chibiImages[2].src="/images/ganyu-chibi-5.png";
+
+    // Change theme to ganyu colored background
+    for(var i = 0; i < menuHeaders.length; i ++){
+        menuHeaders[i].classList.add("bg-ganyu-dark-blue");
+        menuHeaders[i].classList.remove("bg-wanderer-grey-blue-1");
+    }
+
+    // Change color of date header
+    dateHeader[0].classList.remove("bg-wanderer-grey-blue-2");
+    dateHeader[0].classList.add("bg-ganyu-light-red");
+
+    // Change main container color
+    mainContainer[0].classList.remove("bg-wanderer-pastel-blue");
+    mainContainer[0].classList.add("bg-ganyu-pastel-blue");
+};  
+
+function wandererTheme(){
+
+    // Change CSS image source to Ganyu Chibi
+    chibiImages[0].src="/images/wanderer-chibi-1.png";
+    chibiImages[1].src="/images/wanderer-chibi-2.png";
+    chibiImages[2].src="/images/wanderer-chibi-3.png";
+
+    // Change theme to ganyu colored background
+    for(var i = 0; i < menuHeaders.length; i ++){
+        menuHeaders[i].classList.add("bg-wanderer-grey-blue-1");
+        menuHeaders[i].classList.remove("bg-ganyu-dark-blue");
+    }
+
+    // Change color of date header
+    dateHeader[0].classList.remove("bg-ganyu-light-red");
+    dateHeader[0].classList.add("bg-wanderer-grey-blue-2");
+
+    // Change main container color
+    mainContainer[0].classList.remove("bg-ganyu-pastel-blue");
+    mainContainer[0].classList.add("bg-wanderer-pastel-blue");
+};  
