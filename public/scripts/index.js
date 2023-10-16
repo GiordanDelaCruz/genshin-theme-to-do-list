72d006a8e0fd1034ed21db4a136592f4dc72c19c
Giordan Dela Cruz <52592435+GiordanDelaCruz@users.noreply.github.com>
2023-10-16 01:29:57 -0400

    Updated look of Ganyu and Wanderer themes. Need to implement them the scroll bar to change with the theme as well.

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,9 +1,12 @@
 // Get reference to all objects
 const dateHeader = document.getElementsByClassName("date-header");
 const menuHeaders = document.getElementsByClassName("menu-header");
+const inputRectangles = document.getElementsByClassName("input-group-text");
 const chibiImages = document.getElementsByClassName("chibi-character");
 const mainContainer = document.getElementsByClassName("main-container");
 const characterQuote = document.getElementsByClassName("character-quote");
+const footer = document.getElementsByTagName("footer");
+const body = document.getElementsByTagName("body");
 
 // Keep Track Current Theme
 var currentTheme = "ganyu";
@@ -35,15 +38,23 @@ function ganyuTheme(){
     // Change to a random Ganyu Quote
     var randNum = Math.floor( Math.random() * ganyuQuotes.length );
     characterQuote[0].innerHTML = ganyuQuotes[randNum];
+    characterQuote[0].classList.add("text-ganyu-light-red");
+    characterQuote[0].classList.remove("text-wanderer-royal-blue");
 
     // Change color of date header
-    dateHeader[0].classList.remove("bg-wanderer-grey-blue-2");
+    dateHeader[0].classList.remove("bg-wanderer-royal-blue");
     dateHeader[0].classList.add("bg-ganyu-light-red");
-    dateHeader[0].style.borderColor = "red";
+    dateHeader[0].style.borderColor = "white";
 
     // Change main container color
-    mainContainer[0].classList.remove("bg-wanderer-pastel-blue");
+    mainContainer[0].classList.remove("bg-wanderer-light-purple");
     mainContainer[0].classList.add("bg-ganyu-pastel-blue");
+
+    // Change background color of input rectangles
+    for(var i = 0; i < inputRectangles.length; i ++){
+        inputRectangles[i].classList.add("bg-ganyu-light-yellow");
+        inputRectangles[i].classList.remove("bg-wanderer-beige");
+    }  
 
     // Change CSS image source to Ganyu Chibi
     chibiImages[0].src="/images/ganyu-chibi-4.png";
@@ -52,9 +63,18 @@ function ganyuTheme(){
 
     // Change theme to ganyu colored background
     for(var i = 0; i < menuHeaders.length; i ++){
-        menuHeaders[i].classList.remove("bg-wanderer-grey-blue-1");
+        menuHeaders[i].classList.remove("bg-wanderer-teal");
         menuHeaders[i].classList.add("bg-ganyu-dark-blue");
     }
+
+      // Change footer
+      footer[0].classList.add("bg-ganyu-dark-blue");
+      footer[0].classList.remove("bg-wanderer-teal");
+
+      // Change body
+      body[0].classList.add("bg-ganyu-dark-blue");
+      body[0].classList.remove("bg-wanderer-teal");
+      
 };  
 
 function wandererTheme(){
@@ -62,33 +82,49 @@ function wandererTheme(){
 
     // Change to a random Wanderer Quote
     var randNum = Math.floor(Math.random() * wandererQuotes.length);
-    characterQuote[0].innerHTML = wandererQuotes[randNum];
-
+    characterQuote[0].innerHTML = wandererQuotes[randNum];  
+    characterQuote[0].classList.add("text-wanderer-royal-blue");
+    characterQuote[0].classList.remove("text-ganyu-light-red");
+   
     // Change color of date header
     dateHeader[0].classList.remove("bg-ganyu-light-red");
-    dateHeader[0].classList.add("bg-wanderer-grey-blue-2");
-    dateHeader[0].style.borderColor = "#686589";
+    dateHeader[0].classList.add("bg-wanderer-royal-blue");
+    dateHeader[0].style.borderColor = "white";
 
     // Change main container color
     mainContainer[0].classList.remove("bg-ganyu-pastel-blue");
-    mainContainer[0].classList.add("bg-wanderer-pastel-blue");
+    mainContainer[0].classList.add("bg-wanderer-light-purple");
+
+    // Change background color of input rectangles
+    for(var i = 0; i < inputRectangles.length; i ++){
+        inputRectangles[i].classList.add("bg-wanderer-beige");
+        inputRectangles[i].classList.remove("bg-ganyu-light-yellow");
+    }  
 
     // Change CSS image source to Wanderer Chibi
     chibiImages[0].src="/images/wanderer-chibi-1.png";
     chibiImages[1].src="/images/wanderer-chibi-2.png";
     chibiImages[2].src="/images/wanderer-chibi-3.png";
 
-    // Change theme to Wanderer colored background
-    for(var i = 0; i < menuHeaders.length; i ++){
+    // Change Menu headers
+    for(var i = 0; i < menuHeaders.length; i++){
+        menuHeaders[i].classList.add("bg-wanderer-teal");
         menuHeaders[i].classList.remove("bg-ganyu-dark-blue");
-        menuHeaders[i].classList.add("bg-wanderer-grey-blue-1");
     }
+
+    // Change footer
+    footer[0].classList.add("bg-wanderer-teal");
+    footer[0].classList.remove("bg-ganyu-dark-blue");
+
+    // Change body  
+    body[0].classList.add("bg-wanderer-teal");
+    body[0].classList.remove("bg-ganyu-dark-blue");  
 };  
 
 function updateTheme(){
-    if( currentTheme == "ganyu" ){
+    if( currentTheme === "ganyu" ){
         ganyuTheme();
-    }else if( currentTheme == "wanderer"){
+    }else if( currentTheme === "wanderer"){
         wandererTheme();
     }
 }
