57fc65ab51445b0637babffb4524bfdcba6b5ef9
Giordan Dela Cruz <52592435+GiordanDelaCruz@users.noreply.github.com>
2024-03-18 20:02:15 -0400

    Updated to-do-list backend and frontend.

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,17 +1,13 @@
-// Get reference to all objects
-const dateHeader = document.getElementsByClassName("date-header");
-const menuHeaders = document.getElementsByClassName("menu-header");
-const inputRectangles = document.getElementsByClassName("input-group-text");
+// Run after all element in the web page have loaded
+$(document).ready(function(){ 
+    ganyuTheme();
+});
+
+/*************************************************************************/
+/********                      Variables                         *********/
+/*************************************************************************/
 const chibiImages = document.getElementsByClassName("chibi-character");
-const mainContainer = document.getElementsByClassName("main-container");
-const characterQuote = document.getElementsByClassName("character-quote");
-const footer = document.getElementsByTagName("footer");
-const body = document.getElementsByTagName("body");
-
-// Keep Track Current Theme
 var currentTheme = "ganyu";
-
-// Ganyu Quotes
 const ganyuQuotes = [
     `"Next on the agenda..."`,
     `"We're way behind schedule..."`,
@@ -19,8 +15,6 @@ const ganyuQuotes = [
     `"I'm a little low on energy."`, 
     `"Time to begin work?"`
 ];
-
-// Wanderer Quotes
 const wandererQuotes = [
     `"Huh. How irritating."`, 
     `"Cry louder!"`, 
@@ -29,123 +23,66 @@ const wandererQuotes = [
     `"Prepare yourself for punishment."`, 
 ];
 
-$(document).ready(function(){ 
-    // $(".date-header").addClass("bg-ganyu-light-red")
-
- 
-});
-
+/*************************************************************************/
+/********                      Functions                         *********/
+/*************************************************************************/
 function editItem(id){
     // Hide display
-    $("#delete-btn-" + id).hide(1000);
-    $("#edit-btn-" + id).hide(1000);
-    $("#item-display-" + id).hide(1000);
+    $("#delete-btn-" + id).hide(1);
+    $("#edit-btn-" + id).hide(1);
+    $("#item-display-" + id).hide(1);
     // Show edit inputs
-    $("#done-btn-" + id).show(2000);
-    $("#edit-input-" + id).show(2000);
+    $("#edit-input-" + id).show(300, function(){
+        $("#done-btn-" + id).show(16);
+    }); 
+    $("#done-btn-" + id).css("border-radius", "20px 0 0 20px");
+    $("#edit-input-" + id ).each(function () {
+        this.style.setProperty("border-radius", "0 20px 20px 0",  "important");
+    });
 }
-
-// Turn on correct theme at the start
-updateTheme();
-
 function ganyuTheme(){
     currentTheme = "ganyu";
-
-    // Change to a random Ganyu Quote
+    // // Character Quote
     var randNum = Math.floor( Math.random() * ganyuQuotes.length );
-    characterQuote[0].innerHTML = ganyuQuotes[randNum];
-    characterQuote[0].classList.add("text-ganyu-light-red");
-    characterQuote[0].classList.remove("text-wanderer-royal-blue");
-
-    // Change color of date header
-    dateHeader[0].classList.remove("bg-wanderer-royal-blue");
-    dateHeader[0].classList.add("bg-ganyu-light-red");
-    dateHeader[0].style.borderColor = "white";
-
-    // Change main container color
-    mainContainer[0].classList.remove("bg-wanderer-light-purple");
-    mainContainer[0].classList.add("bg-ganyu-pastel-blue-2");
-
-    // Change background color of input rectangles
-    for(var i = 0; i < inputRectangles.length; i ++){
-        inputRectangles[i].classList.add("bg-ganyu-light-yellow");
-        inputRectangles[i].classList.remove("bg-wanderer-beige");
-    }  
-
-    // Change CSS image source to Ganyu Chibi
+    $(".character-quote").text(ganyuQuotes[randNum]);
+    // Chibi Images
     chibiImages[0].src="/images/ganyu-chibi-4.png";
     chibiImages[1].src="/images/ganyu-chibi-3.png";
     chibiImages[2].src="/images/ganyu-chibi-5.png";
-
-    // Change theme to ganyu colored background
-    for(var i = 0; i < menuHeaders.length; i ++){
-        menuHeaders[i].classList.remove("bg-wanderer-teal");
-        menuHeaders[i].classList.add("bg-ganyu-dark-blue");
-    }
-
-      // Change footer
-      footer[0].classList.add("bg-ganyu-dark-blue");
-      footer[0].classList.remove("bg-wanderer-teal");
-
-      // Change body
-      body[0].classList.add("bg-ganyu-light-blue-2");
-      body[0].classList.remove("bg-wanderer-teal");
-      
+    // TODO: Character Button (make it grow larger to show what theme is currently used)
+    // $(".ganyu-btn").addClass()
 };  
-
 function wandererTheme(){
-    currentTheme = "wanderer";
-
-    // Change to a random Wanderer Quote
-    var randNum = Math.floor(Math.random() * wandererQuotes.length);
-    characterQuote[0].innerHTML = wandererQuotes[randNum];  
-    characterQuote[0].classList.add("text-wanderer-royal-blue");
-    characterQuote[0].classList.remove("text-ganyu-light-red");
-   
-    // Change color of date header
-    dateHeader[0].classList.remove("bg-ganyu-light-red");
-    dateHeader[0].classList.add("bg-wanderer-royal-blue");
-    dateHeader[0].style.borderColor = "white";
-
-    // Change main container color
-    mainContainer[0].classList.remove("bg-ganyu-pastel-blue-2");
-    mainContainer[0].classList.add("bg-wanderer-light-purple");
-
-    // Change background color of input rectangles
-    for(var i = 0; i < inputRectangles.length; i ++){
-        inputRectangles[i].classList.add("bg-wanderer-beige");
-        inputRectangles[i].classList.remove("bg-ganyu-light-yellow");
-    }  
-
-    // Change CSS image source to Wanderer Chibi
+    // // Character Quote
+    var randNum = Math.floor( Math.random() * wandererQuotes.length );
+    $(".character-quote").text(wandererQuotes[randNum]);
+    // Chibi Images
     chibiImages[0].src="/images/wanderer-chibi-1.png";
     chibiImages[1].src="/images/wanderer-chibi-2.png";
-    chibiImages[2].src="/images/wanderer-chibi-3.png";
-
-    // Change Menu headers
-    for(var i = 0; i < menuHeaders.length; i++){
-        menuHeaders[i].classList.add("bg-wanderer-teal");
-        menuHeaders[i].classList.remove("bg-ganyu-dark-blue");
-    }
-
-    // Change footer
-    footer[0].classList.add("bg-wanderer-teal");
-    footer[0].classList.remove("bg-ganyu-dark-blue");
-
-    // Change body  
-    body[0].classList.add("bg-wanderer-teal");
-    body[0].classList.remove("bg-ganyu-light-blue-2");  
+    chibiImages[2].src="/images/wanderer-chibi-3.png";  
+    // TODO: Character Button (make it grow larger to show what theme is currently used)
+    // $(".wanderer-btn").addClass()
 };  
 
-function updateTheme(){
-    if( currentTheme === "ganyu" ){
-        ganyuTheme();
-    }else if( currentTheme === "wanderer"){
-        wandererTheme();
+// function to set a given theme/color-scheme
+function setTheme(themeName) {
+    switch(themeName){
+        case 'theme-ganyu-light':
+            ganyuTheme();
+            break;
+        case 'theme-wanderer-light':
+            wandererTheme();
+            break;
     }
+    localStorage.setItem('theme', themeName);
+    document.documentElement.className = themeName;
 }
-
-// function editItem(itemId){
-//     const itemTitle = document.getElementById(itemId);
-//     const editItemInput = document.getElementById(editItemId)
-// }
+(function () {
+   if (localStorage.getItem('theme') === 'theme-ganyu-light') {
+       setTheme('theme-ganyu-light');
+       ganyuTheme();
+   } else if(localStorage.getItem('theme') === 'theme-wanderer-light') {
+       setTheme('theme-wanderer-light');
+       wandererTheme();
+   }
+})();
